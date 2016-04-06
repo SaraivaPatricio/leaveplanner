9f40565aa59e2f7b70477f83ddf8dab0d76d01b3
patricio saraiva <patricio saraiva>
2016-04-06 11:06:31 +0200

    redirection if unlogged
    Signed-off-by: patricio saraiva <patricio saraiva>

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -69,4 +69,17 @@ angular
       });
     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
+  }).run( function($rootScope, $location) {
+  // register listener to watch route changes
+  $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+    if ( $rootScope.userConnected == null ) {
+      // no logged user, we should be going to #login
+      if ( next.templateUrl == "login.html" ) {
+        // already going to #login, no redirect needed
+      } else {
+        // not going to #login, we should redirect now
+        $location.path( "/login" );
+      }
+    }
   });
+});
