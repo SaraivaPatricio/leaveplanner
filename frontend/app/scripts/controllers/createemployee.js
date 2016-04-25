66c9ec24f146a8bd1877214ef78b6368f1c4b67f
patricio saraiva <patricio saraiva>
2016-04-25 09:16:27 +0200

    employee edit relationship
    Signed-off-by: patricio saraiva <patricio saraiva>

--- frontend/app/scripts/controllers/createemployee.js
+++ frontend/app/scripts/controllers/createemployee.js
@@ -8,11 +8,13 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('CreateEmployeeCtrl', function ($scope, $routeParams, $location, userService) {
+  .controller('CreateEmployeeCtrl', function ($scope, $routeParams, $location, userService, departementService) {
 
     $scope.employee = {};
+    $scope.departements = departementService.query();
 
     $scope.saveEmployee = function(employee) {
+      $scope.employee.departement = $scope.departementSelected;
       new userService(employee).$save().then(
         function( value ){$location.path("/employees");},
         function( error ){}
--- frontend/app/scripts/controllers/editemployee.js
+++ frontend/app/scripts/controllers/editemployee.js
@@ -8,16 +8,23 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('EditEmployeeCtrl', function ($scope, $routeParams, $location, userService) {
+  .controller('EditEmployeeCtrl', function ($scope, $routeParams, $location, departementService, userService) {
 
-    $scope.employee = userService.get({ id: $routeParams.id });
+    $scope.employee = userService.get({ id: $routeParams.id }, function() {
+      $scope.departements = departementService.query(function() {
+        angular.forEach($scope.departements, function(value, key){
+          if($scope.employee.departement && value.id == $scope.employee.departement.id)
+            $scope.departementSelected = value;
+        });
+      });
+    });
 
     $scope.saveEmployee = function(employee) {
+      $scope.employee.departement = $scope.departementSelected;
       employee.$update().then(
         function( value ){$location.path("/employees");},
         function( error ){}
       );
-
     };
 
   });
